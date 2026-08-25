2eaca66bce35e172faa70c3cdf3855a282b38238
ahmed <ahmed@oplevel.ai>
2026-08-24 22:42:34 -0400

    Fix custom domain asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: process.env.SITE_URL ?? "https://www.activityschema.com",
+  site: process.env.SITE_URL ?? "https://activityschema.com",
   base: process.env.BASE_PATH ?? "/",
   output: "static",
   build: {
